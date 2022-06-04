157b1930efe9d0061cc159396f5352eacc644b9f
danielcrubens <dcr.tecnologi@gmail.com>
2022-06-04 11:51:09 -0300

    lógica remover, adicionar classes   :construction:

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,50 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+};
+
+// create map
+const map = L.map('mapid', options).setView([-23.4871816, -47.4874388], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L.marker([-23.4871816, -47.4874388], { icon })
+    .addTo(map)
+
+
+//image gallery
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes . active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+    
+    function removeActiveClass(button) {
+        button.classList.remove("active")   
+    }
+    // selecionar a image clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details >img")
+    
+    //atualizar o container da image
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add("active");
+}
