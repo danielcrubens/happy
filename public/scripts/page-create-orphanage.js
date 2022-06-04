e6bfa59c72a758c4e94bbe1844a44f68635d5cee
danielcrubens <dcr.tecnologi@gmail.com>
2022-06-04 17:45:30 -0300

    lógica adicionando  e removendo campos de fotos   :construction:

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,60 @@
+// create map
+const map = L.map('mapid').setView([-23.4871816, -47.4874388], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+})
+
+//create and add marker
+let marker;
+
+map.on("click", (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector("[name=lat]").value = lat;
+    document.querySelector("[name=lng]").value = lng;
+
+    marker && map.removeLayer(marker);
+
+    marker = L.marker([lat, lng], { icon }).addTo(map);
+});
+
+//adicionar campo de fotos
+
+function addPhotoField() {
+    //pegar o container de fotos #images
+    const container = document.querySelector("#images")
+    //pegar o container para duplicar . new-upload
+    const fieldsContainer = document.querySelectorAll(".new-upload")
+    //realizar o clone da última imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    //verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+
+    if (input.value == "") {
+        return
+    }
+    //limpar o campo de adicionar o container de imagens
+    input.value = ""
+    //adcionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget;
+    const fieldsContainer = document.querySelectorAll(".new-upload")
+    if (fieldsContainer.length <= 1) {
+        //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+//deletar o campo
+    span.parentNode.remove()
+}
